ba005339dd3b953a8a50bbdf4433d39d360cec0c
AugRob <auro.001@gmail.com>
2013-09-08 21:46:07 -0500

    Update key-navigation.js

--- key-navigation/key-navigation.js
+++ key-navigation/key-navigation.js
@@ -38,6 +38,48 @@ $(document).ready(function () {
             }
 
         }
-        $(".counter").text(postNumber + " " + onPost);
+        $(".counter").text(postNumber + " " + onPost);$(document).ready(function () {
+    window.postNumber = 0;
+    var onPost = false;
+    var postArray = $(".post").toArray();
+    var postMargin = -8;
+    var postDelay = 100;
+    $(document).on('keydown', function (e) {
+        window.keyCode = e.which;
+        if (postNumber < postArray.length, postNumber >= 0) {
+            //Press 'J'
+            if (e.which == 74) {
+                if (postNumber < postArray.length - 1) {
+                    if (onPost == true && $(window).scrollTop() + $(window).height() != $(document).height()) {   
+                        postNumber = postNumber + 1;
+                        $("html").animate({scrollTop: $(".post").eq(postNumber).position().top + postMargin}, postDelay);
+                    } else {
+                        while ($(".post").eq(postNumber).position().top < $(window).scrollTop()) {
+                            postNumber++;
+                        }
+                        $("html").animate({scrollTop: $(".post").eq(postNumber).position().top + postMargin}, postDelay);
+                        onPost = true;
+                    }
+                }
+            }
+            //Press 'K'
+            else if (e.which == 75) {
+                if (postNumber != 0) {
+                    postNumber = postNumber - 1;
+                    $("html").animate({scrollTop: $(".post").eq(postNumber).position().top + postMargin}, postDelay);
+                    onPost = true;
+                }
+            }
+            if (postNumber < 0) {
+                postNumber = 0;
+            }
+            if (postNumber >= postArray.length) {
+                postNumber = (postArray.length - 1);
+            }
+
+        }
+    });
+});
+
     });
 });
